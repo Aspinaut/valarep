f3e9eafb69fa0ccf348e9dcf661ff9637d9dec49
Vincent Masse <vincentmasse@protonmail.com>
2020-11-30 12:40:04 +0100

    add serious game project

--- stage/form.php
+++ stage/form.php
@@ -1,1 +1,14 @@
+<?php
+try {
+  $bdd = new PDO('mysql:host=localhost;dbname=learnphp;charset=utf8', 'sa', 'admin', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch (\Exception $e) {
+  die("Erreur " . $e->getMessage());
+}
+  $req = $bdd->prepare('INSERT INTO comment(content, author) VALUES(:content, :author)');
+  $req->execute(array(
+    'content' => $_POST['message'],
+    'author' => $_POST['pseudo']
+  ));
 
+  header('Location: index.php');
+?>
--- stage/index.php
+++ stage/index.php
@@ -6,13 +6,32 @@
     <title></title>
   </head>
   <body>
-    <?php echo $_SESSION['name']; ?>
-    <?php include("header.php"); ?>
-    <form class="" action="index.php" method="post">
-      <input type="text" name="name" placeholder="Insert your name here">
-      <?php $_SESSION['name'] = "BOb"; ?>
-      <input type="submit" value="Envoyer !">
+    <h1>Bienvenue sur le chat</h1>
+    <?php
+    try {
+      $bdd = new PDO('mysql:host=localhost;dbname=learnphp;charset=utf8', 'sa', 'admin', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } catch (\Exception $e) {
+      die("Erreur " . $e->getMessage());
+    }
+
+    ?>
+
+    <form class="" action="form.php" method="post">
+      <label for="pseudo">Pseudo : </label><input type="text" name="pseudo">
+      <label for="message">Message : </label><textarea name="message" rows="8" cols="80" placeholder="Your message : "></textarea>
+      <input type="submit" value="Submit">
     </form>
-    <?php include("footer.php"); ?>
+    <?php
+      $req = $bdd->prepare('SELECT * FROM comment ORDER BY ID DESC LIMIT 0,5');
+      $req->execute();
+      while ($message = $req->fetch())
+      {
+        echo "
+          <p><strong> ". htmlspecialchars($message['author']) . "</strong> : </p>
+          <p>". htmlspecialchars($message['content']) . "</p>
+        ";
+      }
+      $req->closeCursor();
+    ?>
   </body>
 </html>
